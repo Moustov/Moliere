1dc99bcf48300b5d876e757e8d1576bc4b220fcd
chris <christophe.moustier@free.fr>
2022-05-16 08:53:50 +0200

    generate_screenplay updated to match the other modules

--- generate_screenplay.py
+++ generate_screenplay.py
@@ -1,18 +1,19 @@
 from extract_objects import extract_screenplay_objects
-from skeleton_generator import generate_skeleton
+from skeleton_generator import SkeletonGenerator
 
 
 def generate_screenplay(a_scene: str):
     my_screenplay_objects = extract_screenplay_objects(a_scene)
-    generate_skeleton(my_screenplay_objects)
+    generator = SkeletonGenerator("output")
+    generator.generate_skeleton_parts(my_screenplay_objects, True)
 
 
 if __name__ == '__main__':
     my_scene = """
-            GIVEN <Jack> who can <browse the web> and <call HTTP APIs> and <go to the pub>
-            WHEN <Jack> does <go to the pub> at <The Sheep's Head Pub>
+            GIVEN <Jack Donald> who can <browse the web> and <call HTTP APIs> and <go to the pub>
+            WHEN <Jack Donald> does <go to the pub> at <The Sheep's Head Pub>
                 AND <order> with <999 beers>
-                THEN <Jack> checks <the total amount> is <999 × 2.59 EUR>
+                THEN <Jack Donald> checks <the total amount> is <999 × 2.59 EUR>
                           THANKS TO <the total amount> FOUND ON <the bill>
             """
     generate_screenplay(my_scene)
