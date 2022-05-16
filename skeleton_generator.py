45357ef93ffb872fde6be9ebe4a629f89a95b99e
chris <christophe.moustier@free.fr>
2022-05-17 01:02:48 +0200

    REFACTORING: doctrings + static method

--- skeleton_generator.py
+++ skeleton_generator.py
@@ -2,21 +2,25 @@ import os
 import shutil
 
 
+def change_to_camel_case(class_name: str):
+    """Turns a class name into PEP8 standard (ie. with CamelCase)
+    :param class_name: a class name
+    """
+    words = class_name.split(" ")
+    res = ""
+    for word in words:
+        res += word[0].upper() + word[1:]
+    return res
+
 
 class SkeletonGenerator:
     def __init__(self, output_dir: str):
         self.output_directory = output_dir
 
-    def change_to_camel_case(self, class_name: str):
-        words = class_name.split(" ")
-        res = ""
-        for word in words:
-            res += word[0].upper() + word[1:]
-        return res
-
     def generate_skeleton(self):
+        """Copy base classes to self.output_directory"""
         self.generate_skeleton_with_base_class("actor")
-        self.generate_skeleton_with_base_class("ability")
+        self.generate_skeleton_with_base_class("ability", "abilities")
         self.generate_skeleton_with_base_class("action")
         self.generate_skeleton_with_base_class("element")
         self.generate_skeleton_with_base_class("fact")
@@ -26,8 +30,16 @@ class SkeletonGenerator:
         shutil.copyfile(os.path.normcase(f"canvas/screenplay.py"),
                         os.path.normcase(f"{self.output_directory}/screenplay.py"))
 
-    def generate_skeleton_with_base_class(self, base_class:str):
-        part_type_folder_name = f"{base_class}s"
+    def generate_skeleton_with_base_class(self, base_class: str, folder_name: str = None):
+        """Copy a base class
+        :param base_class: one of the SPDP base class
+        :param folder_name: this string is to be used as the destination folder
+        """
+        part_type_folder_name = ""
+        if folder_name is not None:
+            part_type_folder_name = folder_name
+        else:
+            part_type_folder_name = f"{base_class}s"
         if not os.path.exists(self.output_directory + f"/{part_type_folder_name}"):
             os.mkdir(self.output_directory + f"/{part_type_folder_name}")
             print("Directory ", self.output_directory + f"/{part_type_folder_name}", " Created ")
@@ -37,30 +49,40 @@ class SkeletonGenerator:
         shutil.copyfile(os.path.normcase(f"canvas/{base_class.lower()}.py"),
                         os.path.normcase(f"{self.output_directory}/{part_type_folder_name}/{base_class.lower()}.py"))
 
-    def generate_skeleton_part(self, part: dict, part_type: str, regenerate_project: bool):
+    def generate_skeleton_part(self, the_part: dict, part_type: str, regenerate_project: bool):
+        """Generate the classes from the_part
+        :param regenerate_project:
+        :param part_type:
+        :param the_part:
+        """
         part_type_folder_name = f"{part_type.lower()}s"
         if not os.path.exists(self.output_directory + f"/{part_type_folder_name}"):
             os.mkdir(self.output_directory + f"/{part_type_folder_name}")
             print("Directory ", self.output_directory + f"/{part_type_folder_name}", " Created ")
         shutil.copyfile(os.path.normcase(f"canvas/{part_type.lower()}.py"),
                         os.path.normcase(f"{self.output_directory}/{part_type_folder_name}/{part_type.lower()}.py"))
-
-        for class_name in part:
+        for class_name in the_part:
             class_canvas = ""
-            class_name = self.change_to_camel_case(class_name)
+            class_name = change_to_camel_case(class_name)
             with open(os.path.normcase("canvas/class_canvas.py"), "r") as class_canvas_file:
                 class_canvas = class_canvas_file.read()
             class_canvas = class_canvas.replace("TheClassType", part_type)
             class_canvas = class_canvas.replace("TheClass", class_name)
             class_canvas = class_canvas.replace("#YOUR IMPORTS",
                                                 f"from {self.output_directory}.{part_type_folder_name}.{part_type.lower()} import {part_type}")
-            with open(os.path.normcase(f"{self.output_directory}/{part_type_folder_name}/{class_name}.py"), "w") as class_file:
+            with open(os.path.normcase(f"{self.output_directory}/{part_type_folder_name}/{class_name}.py"),
+                      "w") as class_file:
                 class_file.write(class_canvas)
 
-
-    def generate_skeleton_complex_part(self, param, param1, regenerate_project):
+    def generate_skeleton_complex_part(self, the_part: dict , part_type: str, regenerate_project: bool):
+        """
+        same as generate_skeleton_part except it handles objects with parameters such as questions, elements, abilities and actoins
+        :param the_part:
+        :param part_type:
+        :param regenerate_project:
+        :return:
+        """
         pass
-
 
     def generate_skeleton_parts(self, screenplay_objects: dict, regenerate_project: bool = False):
         """
