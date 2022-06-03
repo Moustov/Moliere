1145e3ffa7f44fb689400cfae914edfca2b4824d
chris <christophe.moustier@free.fr>
2022-06-03 23:02:53 +0200

    REFACTOR : tasks canvas adding actions attempt

--- canvas/task.py
+++ canvas/task.py
@@ -1,10 +1,11 @@
 from canvas.action import Action
 from canvas.actor import Actor
+from canvas.element import Element
 
 
 class Task:
     def __init__(self, name: str):
         self.made_of = []
 
-    def made_up_of(self, an_actor: Actor):
-        pass
+    def made_up_of(self, an_actor: Actor, an_element: Element, a_param: str):
+        self.made_of.append({"actor": an_actor, "what": an_element, "value": a_param})
